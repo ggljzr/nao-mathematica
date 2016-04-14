95fc100756890918c1a56ef34ef0620153a75876
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-04-14 16:08:06 +0200

    neco dalsiho lol

--- sudoku.py
+++ sudoku.py
@@ -14,6 +14,7 @@ if argc > 1:
 	image_path = str(sys.argv[1])
 
 img = cv2.imread(image_path)
+rows, cols, ch = img.shape
 
 #nejdriv prevedu na grayscale a vykreslim
 #kontury podel hran (canny)
@@ -21,7 +22,7 @@ img = cv2.imread(image_path)
 #a je lepsi je udelat silnejsi (2px)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-gray = cv2.GaussianBlur(gray, (5,5), 0)
+#gray = cv2.GaussianBlur(gray, (5,5), 0)
 thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -43,9 +44,30 @@ for point in biggest:
 	x,y = point[0].ravel()
 	cv2.circle(img,(x,y),4,255,-1)
 
+print biggest[0][0]
 
+top_left = biggest[0][0]
+bottom_left = biggest[1][0]
+bottom_right = biggest[2][0]
+top_right = biggest[3][0]
 
-cv2.imshow('dst', thresh) #dst je uz vyrovnanej vobrazek, sou v nem teda este vykreselny primky a rohy
+pts1 = np.float32([top_left,bottom_left,top_right,bottom_right])
+pts2 = np.float32([[0,0],[0,rows],[cols,0],[cols,rows]])
+
+M = cv2.getPerspectiveTransform(pts1,pts2)
+dst = cv2.warpPerspective(gray,M,(rows,cols))
+
+#mozna taky zkusit
+# https://stackoverflow.com/questions/23506105/extracting-text-opencv
+#pro detekci regionu s textem
+
+canny_output = cv2.Canny(dst, 60,200)
+contours, hierarchy = cv2.findContours(canny_output,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+#cv2.drawContours(dst,contours,-1,(255,255,255),1)
+thresh1 = cv2.adaptiveThreshold(dst,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+
 cv2.imshow('img', img) #puvodni vobrazek kde sou doplneny primky a rohy
+#cv2.imshow('dst', dst) #dst je uz vyrovnanej vobrazek, sou v nem teda este vykreselny primky a rohy
+cv2.imshow('thresh', thresh1)
 cv2.waitKey()
 
