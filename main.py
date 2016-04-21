d146be3422ad3be33362a65110bda8a51561a8c4
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-04-21 15:35:05 +0200

    added get_text_regions function in utils, added notes

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-import whiteboard as wb
-import sys
+#https://stackoverflow.com/questions/23506105/extracting-text-opencv
+#modified answer 3, added gaussian blur and adaptive threshold
+#pro ziskani strokes je asi potreba neco jako
+#https://en.wikipedia.org/wiki/Topological_skeleton, coz neni v opencv
+#distance transformation
+#clusterizace
+
+#este by to teda chtelo udelat nakou tu transformaci tim morpholgy operatorem
+#(dilatace a tak), protoze u toho vobrazku pod uhlem je pak treba ta sedmicka 
+#takova potrhana coz by asi chtelo zacelit, i kdyz ten seshat by to mozna vydrzel
+
+#jinak co se tyce tech vobrazku tak to numpy.nonzero vypada dobre, akorat by to chtelo
+#rozrezat ten text region este na jednotlivy znaky zase pomoci kontur a bounding boxes
+#aby byly ty jednotlivy strokes jakoby voddeleny 
+
+#^^to asi nebude potreba
+
+#a pak delat ten kazdej znak zvlast s tim, ze by se udaly ty cerny body pomoci
+#toho nonzero a pak by se udal nakej clustering (naky knn nebo takovy nesmysly)
+#co by dal ty sousedni body dohromady
+
+#hledani souvisle oblasti:
+#nejdriv teda asi musime najit souvislou voblast ktera bude tvorit jeden tah
+#takze vzit prvni bilej bod a pak projit vsechny sousedni body a hledat sousedy
+
+#ale stejne by to pak chtelo ty jednotlivy body shluknout do vetsich aby treba nebylo vic bodu tahu
+#na jednim radku, to pak dela v tim seshatu celkem bordel
+
+#este se taky podivat jesli by se nedalo nak pouzit to hierarchalClustering z opencv,
+#najit nakej tutorial
+
+import utils
 import cv2
-import numpy
+import sys
+import numpy as np
 
 image_path = "images/orig.png"
 
@@ -10,15 +41,28 @@ argc = len(sys.argv)
 if argc > 1:
 	image_path = str(sys.argv[1])
 
+
 img = cv2.imread(image_path)
-thresh_final = wb.whiteboard_detect(img)
 
-cv2.imshow("img", img)
-cv2.imshow("thresh_final", thresh_final)
-cv2.imwrite('dst.png', thresh_final)
+text_regions = utils.get_text_regions(img) 
 
-#ted udelat naky to vyznaceni bloku textu
-#https://stackoverflow.com/questions/23506105/extracting-text-opencv
+reg_n = 0
+for region in text_regions:
+    cv2.imshow('region {}'.format(reg_n), region)
+    cv2.imwrite('region{}.png'.format(reg_n), region)
+    reg_n += 1
 
-cv2.waitKey()
+points_array = np.nonzero(text_regions[0])
+points_xy = []
 
+points_xy = zip(points_array[1], points_array[0])
+
+print len(points_xy)
+
+while True:
+    k = cv2.waitKey(33)
+    if k == utils.KEY_Q:
+        break
+    elif k == -1:
+        continue
+ 
--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import cv2
 
 KEY_Q = 1048689
 
+
 def whiteboard_detect(img):
     rows, cols, ch = img.shape
 
@@ -62,10 +63,51 @@ def whiteboard_detect(img):
         # https://stackoverflow.com/questions/23506105/extracting-text-opencv
         # pro detekci regionu s textem
 
-        #thresh_final = cv2.adaptiveThreshold(
-        #    dst, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        # thresh_final = cv2.adaptiveThreshold(
+        # dst, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
     return dst
+
+
+def get_text_regions(img):
+    leveled = whiteboard_detect(img)
+    
+    #rgb = cv2.cvtColor(leveled, cv2.COLOR_GRAY2RGB)
+
+    thresh = cv2.GaussianBlur(leveled, (11, 11), 0)
+    thresh = cv2.adaptiveThreshold(
+        thresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    dilated = cv2.dilate(thresh, kernel, iterations=15)
+    contours, hierarchy = cv2.findContours(
+        dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    rows, cols = thresh.shape
+
+    text_regions = []
+    
+    # for each contour found, draw a rectangle around it on original image
+    for contour in contours:
+        # get rectangle bounding contour
+        [x, y, w, h] = cv2.boundingRect(contour)
+
+        #discard areas that are too big
+        if h > rows - 10 or w > cols - 10:
+            continue
+
+        # discard areas that are too small
+        if h < 60 or w < 60:
+            continue
+
+        # draw rectangle around contour on original image
+        #cv2.rectangle(rgb, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        
+        #crop original leveled image and threshold it
+        rect_mat = leveled[ y : y + h, x : x + w]
+        rect_mat = cv2.adaptiveThreshold(rect_mat, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+        text_regions.append(rect_mat)
+
+    return text_regions
 
 
 def contours_to_scgink(contours, scgink_file, min_length=9):
