c10dbd68b2146bfd354cb1b899c344a05d0924e2
Ondřej Červenka <gogo.lejzr@gmail.com>
2017-09-13 15:26:15 +0200

    Notes about usage (myscript and seshat)

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python
 
 # https://stackoverflow.com/questions/23506105/extracting-text-opencv
-# pro ziskani strokes je asi potreba neco jako
-# https://en.wikipedia.org/wiki/Topological_skeleton, coz neni v opencv
-# distance transformation
-# clusterizace
-# stackoverflow -> finding continuous areas of bits in 2d bit array
+# for access to myscript api you will need the api key from https://developer.myscript.com/
 
-# este by to teda chtelo udelat nakou tu transformaci tim morpholgy operatorem
-#(dilatace a tak), protoze u toho vobrazku pod uhlem je pak treba ta sedmicka
-# takova potrhana coz by asi chtelo zacelit, i kdyz ten seshat by to mozna
-# vydrzel
+# key is then loaded from a .ini file with following structure:
+# [myscript]
+# api_key = <your api key>
 
-# jinak co se tyce tech vobrazku tak to numpy.nonzero vypada dobre, akorat by to chtelo
-# rozrezat ten text region este na jednotlivy znaky zase pomoci kontur a bounding boxes
-# aby byly ty jednotlivy strokes jakoby voddeleny
-
-#^^to asi nebude potreba
-
-# a pak delat ten kazdej znak zvlast s tim, ze by se udaly ty cerny body pomoci
-# toho nonzero a pak by se udal nakej clustering (naky knn nebo takovy nesmysly)
-# co by dal ty sousedni body dohromady
-
-# hledani souvisle oblasti:
-# nejdriv teda asi musime najit souvislou voblast ktera bude tvorit jeden tah
-# takze vzit prvni bilej bod a pak projit vsechny sousedni body a hledat
-# sousedy
-
-# ale stejne by to pak chtelo ty jednotlivy body shluknout do vetsich aby treba nebylo vic bodu tahu
-# na jednim radku, to pak dela v tim seshatu celkem bordel
-
-# este se taky podivat jesli by se nedalo nak pouzit to hierarchalClustering z opencv,
-# najit nakej tutorial
-
-#jako na to hledani tech tahu asi nakonec udelat tu dist transform a pak to hledat podle tech hrebenu
+# if you want to use seshat for recognition (function img_to_latex()), you'll need to get it
+# from https://github.com/falvaro/seshat, build it (note the compile error mentioned in issues)
+# and specify the path to seshat folder in utils.py
 
 import utils
 import processing as proc
